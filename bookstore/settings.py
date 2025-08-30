f4f5be563fc682719224f87583cf8c799cb50457
Matheus8013 <132845674+Matheus8013@users.noreply.github.com>
2025-08-30 17:03:00 -0300

    Update settings.py
    Removendo o django toolbar

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_extensions",
-    "debug_toolbar",
+    #"debug_toolbar",
     "rest_framework",
     "rest_framework.authtoken",
     "order",
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    #"debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "bookstore.urls"
